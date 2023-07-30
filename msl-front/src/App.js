64b7bf223430cf08a0298ae4bdd71e397f214744
FOREST Benjamin <benjamin.frt28@gmail.com>
2023-07-30 23:39:14 +0200

    :sparkles: Initialisation tailwind/daisy UI

--- msl-front/src/App.js
+++ msl-front/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mockup-code">
+      <pre data-prefix="1">
+        <code>npm i daisyui</code>
+      </pre>
+      <pre data-prefix="2">
+        <code>installing...</code>
+      </pre>
+      <pre data-prefix="3" className="bg-warning text-warning-content">
+        <code>Error!</code>
+      </pre>
     </div>
   );
 }
-
-export default App;
